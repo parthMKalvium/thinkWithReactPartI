ae6244ec0f8424a55940fcf982411f4dcc4ecf13
parthMKalvium <147126611+parthMKalvium@users.noreply.github.com>
2023-10-30 12:28:43 +0530

    files added

---
+++ app.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('react-container');
+// ReactDOM.render("Hello! Welcome to React",container);
+
+// const myComponent = ()=>{
+//     return(
+//         React.createElement("div", null,"Hey Kalvians! Welcome to React Learning", React.createElement('div', null, "Let's rock and roll"))
+//     )
+// }
+
+// ReactDOM.render(myComponent(), container);
+
+class ReactContainer extends React.Component{
+    // constructor - to initialize the state 
+  
+    // render method to render the react dom 
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+
+ReactDOM.render(React.createElement(ReactContainer),container);
